2fd7522a1cea7c110727231a400c832273940c15
lortiz24 <luis.ortiz@evius.co>
2023-04-13 23:42:05 -0500

    Se muestran la lista de datos que viene de firebase

--- app/src/main/java/com/example/mypricelist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mypricelist/ui/home/HomeFragment.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBindings
+import com.example.mypricelist.AdaptadorProductList
 import com.example.mypricelist.ProductList
 import com.example.mypricelist.R
 import com.example.mypricelist.databinding.FragmentHomeBinding
 import com.example.mypricelist.ui.creation.CreateListActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.CollectionReference
 
@@ -23,34 +24,40 @@ class HomeFragment : Fragment() {
     val db = FirebaseFirestore.getInstance()
     val coleccion: CollectionReference = db.collection("ListMain")
     private var _binding: FragmentHomeBinding? = null
-    val recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerListas)
-    val addButton = view?.findViewById<FloatingActionButton>(R.id.btnAddList)
     private val listadoList:ArrayList<ProductList> = ArrayList<ProductList>()
     private val binding get() = _binding!!
+    private var adapter:AdaptadorProductList?=null
+    private var recycler: RecyclerView?=null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        leerListadoList()
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val addButton = root.findViewById<FloatingActionButton>(R.id.btnAddList)
-
         addButton.setOnClickListener {
             val createListView = Intent(root.context,CreateListActivity::class.java)
             startActivity(createListView)
         }
 
+        adapter = AdaptadorProductList(listadoList)
+        recycler = root.findViewById(R.id.recyclerListas)
 
+        val layourManager:RecyclerView.LayoutManager = LinearLayoutManager(requireContext().applicationContext)
+
+        recycler?.layoutManager = layourManager
+        recycler?.itemAnimator = DefaultItemAnimator()
+        recycler?.adapter = adapter
         return root
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        leerListadoList()
+    }
     fun leerListadoList(){
         listadoList.clear()
         coleccion.get()
@@ -60,6 +67,7 @@ class HomeFragment : Fragment() {
                         val nuevaList:ProductList = ProductList(""+document.getString("name"),"10")
                         listadoList.add(nuevaList)
                     }
+                    adapter?.notifyDataSetChanged()
                 }
             }
     }
@@ -69,5 +77,3 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
-
-
